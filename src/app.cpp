024f1325d0da09feb1766e5c69e80847be1f9dbc
NiskuT <qjupi63@gmail.com>
2023-07-11 14:13:20 +0200

    Add a keyboard controller

--- src/app.cpp
+++ src/app.cpp
@@ -2,6 +2,7 @@
 
 #include "renderSystem.hpp"
 #include "camera.hpp"
+#include "keyboardController.hpp"
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -9,7 +10,10 @@
 #include <glm/gtc/constants.hpp>
 
 #include <stdexcept>
+#include <chrono>
 #include <array>
+
+#define MAX_FRAME_TIME 0.05f
 
 namespace engine 
 {
@@ -25,12 +29,25 @@ namespace engine
     {
         renderSystem renderSystem{device, engineRenderer.getSwapChainRenderPass()};
         camera myCamera{};
-        //myCamera.setViewDirection(glm::vec3(0.f), glm::vec3(0.5f, 0.f, 1.f));
         myCamera.setViewTarget(glm::vec3(-1.f, -2.f, 2.f), glm::vec3(0.f, 0.f, 2.5f));
+
+        auto viewerObject = gameObject::createGameObject();
+        keyboardController cameraController{};
+
+        auto currentTime = std::chrono::high_resolution_clock::now();
         
         while(!window.shouldClose())
         {
             glfwPollEvents();
+
+            auto newTime = std::chrono::high_resolution_clock::now();
+            float frameTime = std::chrono::duration<float, std::chrono::seconds::period>(newTime - currentTime).count();
+            currentTime = newTime;
+
+            frameTime = std::min(frameTime, MAX_FRAME_TIME);
+
+            cameraController.moveInPlaneXZ(window.getGLFWwindow(), frameTime, viewerObject);
+            myCamera.setViewYXZ(viewerObject.transform.translation, viewerObject.transform.rotation);
 
             float aspect = engineRenderer.getAspectRatio();
             myCamera.setOrthographicProjection(-aspect, aspect, -1, 1, -1, 1);
